09beaba2d90891b7e577bc86258530fadfff846c
Vitor Buzinaro <funny.hc@gmail.com>
2015-09-14 15:32:54 -0300

    Update ie9-oninput-polyfill.js

--- ie9-oninput-polyfill.js
+++ ie9-oninput-polyfill.js
@@ -6,7 +6,7 @@
   d.addEventListener('selectionchange', function() {
     var actEl = d.activeElement;
 
-    if (actEl.tagName.toLowerCase() === 'input' && actEl.type.toLowerCase() === 'text') {
+    if (actEl.tagName === 'INPUT' && actEl.type === 'text') {
       var idx = elements.indexOf(actEl), el = elements[idx] || elements.push(actEl);
       if (el.value !== values[idx]) {
         values[idx] = el.value;
