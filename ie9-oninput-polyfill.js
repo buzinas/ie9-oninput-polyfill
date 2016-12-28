4210d06e803ed0282135c6a2ea2c93c714a3db5e
Vitor Buzinaro <funny.hc@gmail.com>
2016-12-28 17:42:09 -0300

    fixed not creating a new event on selectionchange

--- ie9-oninput-polyfill.js
+++ ie9-oninput-polyfill.js
@@ -1,18 +1,13 @@
 (function (d) {
   if (navigator.userAgent.indexOf('MSIE 9') === -1) return;
-  var elements = [], values = [];
 
   d.addEventListener('selectionchange', function() {
-    var actEl = d.activeElement;
+    var el = d.activeElement;
 
-    if (actEl.tagName === 'TEXTAREA' || (actEl.tagName === 'INPUT' && actEl.type === 'text')) {
-      var idx = elements.indexOf(actEl), el = elements[idx] || elements.push(actEl);
-      if (el.value !== values[idx]) {
-        values[idx] = el.value;
-        var ev = d.createEvent('CustomEvent');
-        ev.initCustomEvent('input', true, true, {});
-        el.dispatchEvent(ev);
-      }
+    if (el.tagName === 'TEXTAREA' || (el.tagName === 'INPUT' && el.type === 'text')) {
+      var ev = d.createEvent('CustomEvent');
+      ev.initCustomEvent('input', true, true, {});
+      el.dispatchEvent(ev);
     }
   });
 })(document);
